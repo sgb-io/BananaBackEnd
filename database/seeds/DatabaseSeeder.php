d55676592581275e5df6cba65044bfec4472a85c
Sam Brown <sam@samuelgbrown.com>
2015-09-02 10:01:20 +0100

    Ignore sqlite database. Added seeds for some dummy sweeties and orders.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(SweetySeeder::class);
+        $this->call(OrderSeeder::class);
 
         Model::reguard();
     }
---
+++ database/seeds/OrderSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+      DB::table('orders')->truncate();
+      $sweeties = App\Sweety::all();
+
+      $orders = [
+        [$sweeties->random(), $sweeties->random()],
+        [$sweeties->random(), $sweeties->random()],
+        [$sweeties->random(), $sweeties->random()],
+        [$sweeties->random(), $sweeties->random()],
+        [$sweeties->random(), $sweeties->random()],
+      ];
+
+      foreach ($orders as $order) {
+        $newOrder = new App\Order;
+        $newOrder->setTotal($order[0]->price + $order[1]->price);
+        $newOrder->save();
+        foreach ($order as $sweety) {
+          $newOrder->sweeties()->attach(App\Sweety::findOrFail($sweety->id));
+        }
+        $newOrder->save();
+      }
+    }
+}
